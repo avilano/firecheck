357f0993681cdfeeb6b6ae5f955e66d0890f32cf
avilano <vira0092@gmail.com>
2016-08-04 14:53:09 -0500

    added requiered to the splash page

--- routes/index.js
+++ routes/index.js
@@ -18,17 +18,16 @@ router.get('/splash', function(req, res, next) {
 });
 // Hanlde POST
 router.post('/splash', function (req, res) {
-
     if (req.body.vechiclePicker == 'truck') {
-      console.log('its a truck!');
+      console.log('user selected truck');
       res.redirect('/truck');
     }
-    if (req.body.vechiclePicker == 'brush') {
-      console.log('its a brush!');
+    else if (req.body.vechiclePicker == 'brush') {
+      console.log('user selected brush');
       res.redirect('/brush');
     }
-    if (req.body.vechiclePicker == 'rescue') {
-      console.log('its a rescue!');
+    else if (req.body.vechiclePicker == 'rescue') {
+      console.log('user selected rescue');
       res.redirect('/rescue');
     }
 });
